24aeffcf0b50e325fb57d09c7d7f8410575a3f10
jrtx0 <jirentianxiang1024@gmail.com>
2023-09-08 01:54:02 +0000

    change meta.js theme to light

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -64,7 +64,7 @@ module.exports = async (data) => {
     bodyClasses: bodyClasses.join(" "),
     noteIconsSettings,
     timestampSettings,
-    baseTheme: process.env.BASE_THEME || "dark",
+    baseTheme: process.env.BASE_THEME || "light",
     siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
     siteBaseUrl: baseUrl,
     styleSettingsCss,
