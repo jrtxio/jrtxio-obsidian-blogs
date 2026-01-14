326e6cc43d62c502dc27c8f68cb8c6a108f6fead
ren.ji <ren.ji@innolight.com>
2026-01-14 11:08:23 +0800

    add ads

--- .eleventy.js
+++ .eleventy.js
@@ -586,6 +586,9 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
+  eleventyConfig.addPassthroughCopy("src/site/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/site/ads.txt");
+  eleventyConfig.addPassthroughCopy("src/site/baidu_verify_codeva-DTBvg2CXkR.html");
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
