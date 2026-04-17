6e4ab5e4a2d8aa6466c504a43861aa2a2ea76fc6
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-04-17 15:32:23 +0800

    Add sortByCreated Nunjucks filter for sorting articles by dg-note-properties.created

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,13 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  eleventyConfig.addNunjucksFilter("sortByCreated", (notes) => {
+    return notes.slice().sort((a, b) => {
+      const dateA = a.data?.["dg-note-properties"]?.created || "";
+      const dateB = b.data?.["dg-note-properties"]?.created || "";
+      return dateB.localeCompare(dateA);
+    });
+  });
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
